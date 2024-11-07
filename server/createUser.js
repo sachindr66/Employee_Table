8851a46eb4c1d41ad1751b848e1c44b26f5a05dc
sachindr66 <sachindrhsn@gmail.com>
2024-11-07 17:50:01 +0530

    first commit

--- server/createUser.js
+++ server/createUser.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 require('dotenv').config();
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -13,10 +12,10 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch((error) => console.error('Could not connect to MongoDB:', error));
 
 const addUser = async () => {
-  const password = 'password123'; // Replace with a secure password
+  const password = 'sachindr@123'; 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const newUser = new User({ userName: 'admin', password: hashedPassword });
+  const newUser = new User({ userName: 'sachindr', password: hashedPassword });
   await newUser.save();
   console.log('Admin user created with hashed password');
   mongoose.connection.close();
